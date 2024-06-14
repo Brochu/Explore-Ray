a2ea2b7912b76d522e60105a00a74151b8584011
Brochu <alexbrochu1@gmail.com>
2024-06-14 11:52:20 -0400

    [DEV] Less dumb way to show all icons

--- src/main.c
+++ src/main.c
@@ -7,8 +7,9 @@
 #define WIDTH 800
 #define HEIGHT 800
 
-#define BUTTON_TEXT "#%i# Icon %i"
-#define BUTTON_WIDTH 75
+#define B_TEXT "#%i# Icon %i"
+#define B_WIDTH 75.f
+#define B_HEIGHT 30.f
 
 int main(int argc, char **argv) {
     printf("[MAIN] Program START\n");
@@ -20,87 +21,16 @@ int main(int argc, char **argv) {
         BeginDrawing();
 
         ClearBackground(BLACK);
-        DrawText("Hello to first Raylib window", 0, 0, 20, LIGHTGRAY);
+        DrawText("Hello to first Raylib window", 0, 750, 20, LIGHTGRAY);
 
         char buttonText[255];
-        for (int i = 1; i <= 25; i++) {
-            int offset = i;
-            sprintf_s(buttonText, 255, BUTTON_TEXT, offset, offset);
+        for (int i = 1; i <= 222; i++) {
+            int col = i / 25;
+            int row = i % 25;
+            sprintf_s(buttonText, 255, B_TEXT, i, i);
 
-            if (GuiButton((Rectangle){25, (i * 30), BUTTON_WIDTH, 30}, buttonText)) {
-                printf("CLICKED on index = %i!\n", offset);
-            }
-        }
-
-        for (int i = 1; i <= 25; i++) {
-            int offset = i + 25;
-            sprintf_s(buttonText, 255, BUTTON_TEXT, offset, offset);
-
-            if (GuiButton((Rectangle){100, (i * 30), BUTTON_WIDTH, 30}, buttonText)) {
-                printf("CLICKED on index = %i!\n", offset);
-            }
-        }
-
-        for (int i = 1; i <= 25; i++) {
-            int offset = i + 50;
-            sprintf_s(buttonText, 255, BUTTON_TEXT, offset, offset);
-
-            if (GuiButton((Rectangle){175, (i * 30), BUTTON_WIDTH, 30}, buttonText)) {
-                printf("CLICKED on index = %i!\n", offset);
-            }
-        }
-
-        for (int i = 1; i <= 25; i++) {
-            int offset = i + 75;
-            sprintf_s(buttonText, 255, BUTTON_TEXT, offset, offset);
-
-            if (GuiButton((Rectangle){250, (i * 30), BUTTON_WIDTH, 30}, buttonText)) {
-                printf("CLICKED on index = %i!\n", offset);
-            }
-        }
-
-        for (int i = 1; i <= 25; i++) {
-            int offset = i + 100;
-            sprintf_s(buttonText, 255, BUTTON_TEXT, offset, offset);
-
-            if (GuiButton((Rectangle){325, (i * 30), BUTTON_WIDTH, 30}, buttonText)) {
-                printf("CLICKED on index = %i!\n", offset);
-            }
-        }
-
-        for (int i = 1; i <= 25; i++) {
-            int offset = i + 125;
-            sprintf_s(buttonText, 255, BUTTON_TEXT, offset, offset);
-
-            if (GuiButton((Rectangle){400, (i * 30), BUTTON_WIDTH, 30}, buttonText)) {
-                printf("CLICKED on index = %i!\n", offset);
-            }
-        }
-
-        for (int i = 1; i <= 25; i++) {
-            int offset = i + 150;
-            sprintf_s(buttonText, 255, BUTTON_TEXT, offset, offset);
-
-            if (GuiButton((Rectangle){475, (i * 30), BUTTON_WIDTH, 30}, buttonText)) {
-                printf("CLICKED on index = %i!\n", offset);
-            }
-        }
-
-        for (int i = 1; i <= 25; i++) {
-            int offset = i + 175;
-            sprintf_s(buttonText, 255, BUTTON_TEXT, offset, offset);
-
-            if (GuiButton((Rectangle){550, (i * 30), BUTTON_WIDTH, 30}, buttonText)) {
-                printf("CLICKED on index = %i!\n", offset);
-            }
-        }
-
-        for (int i = 1; i <= 25; i++) {
-            int offset = i + 200;
-            sprintf_s(buttonText, 255, BUTTON_TEXT, offset, offset);
-
-            if (GuiButton((Rectangle){625, (i * 30), BUTTON_WIDTH, 30}, buttonText)) {
-                printf("CLICKED on index = %i!\n", offset);
+            if (GuiButton((Rectangle){(col * B_WIDTH), (row * B_HEIGHT), B_WIDTH, B_HEIGHT}, buttonText)) {
+                printf("CLICKED on index = %i!\n", i);
             }
         }
 
