7d2067e57006f973cb0885dd632d10bab994b731
Brochu <alexbrochu1@gmail.com>
2024-06-16 00:44:03 -0400

    [DEV] Start work on catalog parsing

--- src/main.c
+++ src/main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 
 #include "iconswatch.h"
+#include "catalog.h"
+
 #include "raylib.h"
 #define RAYGUI_IMPLEMENTATION
 #include "raygui.h"
@@ -10,7 +12,11 @@
 
 int main(int argc, char **argv) {
     printf("[MAIN] Program START\n");
+    printf("[MAIN] Parsing particle effects catalog...\n");
+    parseCatalog();
+    printf("[MAIN] DONE\n");
 
+    printf("[MAIN] Drawing main window...\n");
     InitWindow(WIDTH, HEIGHT, "[ALPHA] Raylib Exploration");
     SetTargetFPS(60);
     GuiLoadStyle("styles/style_cyber.rgs");
@@ -26,6 +32,7 @@ int main(int argc, char **argv) {
 
     CloseWindow();
 
+    printf("[MAIN] DONE\n");
     printf("[MAIN] Program END\n");
     return 0;
 }
