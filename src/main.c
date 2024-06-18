33887a452584be20a520b5e0a1d2d3b09ce0ccbe
Brochu <alexbrochu1@gmail.com>
2024-06-18 13:12:19 -0400

    [DEV] WIP: Started testing libyaml in side program

--- src/main.c
+++ src/main.c
@@ -5,14 +5,28 @@
 #include "raylib.h"
 #define RAYGUI_IMPLEMENTATION
 #include "raygui.h"
+#include "yaml.h"
 
 #define WIDTH 800
 #define HEIGHT 600
 
 int main(int argc, char **argv) {
     printf("[MAIN] Program START\n");
-    printf("[MAIN] Drawing main window...\n");
+    if (argc > 1 && strcmp(argv[1], "-yaml") == 0) {
+        const char *path = "gundata/BombExplo_1EF973EA.gpsm.yaml";
+        printf("Loading yaml at '%s'\n", path);
 
+        FILE *file;
+        fopen_s(&file, path, "rb");
+        char buffer[64];
+        fgets(buffer, 64, file);
+        printf("READ FROM FILE : '%s'\n", buffer);
+
+        fclose(file);
+        return 0;
+    }
+
+    printf("[MAIN] Drawing main window...\n");
     InitWindow(WIDTH, HEIGHT, "[ALPHA] Raylib Exploration");
     InitParticleViewer();
 
