438a2c20d63a31cf0021c3ea31c874700a797b1d
Brochu <alexbrochu1@gmail.com>
2024-06-19 17:27:48 -0400

    [NOTES] Next steps for PART parsing, handle different queries

--- include/partfx.h
+++ include/partfx.h
@@ -3,6 +3,12 @@ typedef struct {
     int lifetime;
 } partfx_t;
 
+//TODO: Find a way to handle different types of queries for values
+// CNST
+// RAND
+// LERP
+// etc..
+
 void partfx_init(partfx_t *pfx);
 void partfx_parse(partfx_t *pfx, const char *data, size_t length);
 void partfx_delete(partfx_t *pfx);
