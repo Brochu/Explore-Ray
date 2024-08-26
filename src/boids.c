9c442c498aaf236c07058da20aee2d061fb6de31
Brochu <alexbrochu1@gmail.com>
2024-08-26 00:13:59 -0400

    [TRAK] Added tracy calls for boids app, also test usage of allocs

--- src/boids.c
+++ src/boids.c
@@ -4,6 +4,7 @@
 #include "raylib.h"
 #include "raygui.h"
 #include "raymath.h"
+#include "TracyC.h"
 
 #define RECT(x, y, w, h) ((Rectangle){x, y, w, h})
 #define NUM_BOIDS 15
@@ -43,6 +44,9 @@ Vector3 camPos = { 0.f, 3.f, 25.f };
 Vector3 camTarget = { 0.f, 0.f, 0.f };
 Vector3 camUp = { 0.f, 1.f, 0.f };
 
+size_t count = 50;
+int *arr = NULL;
+
 typedef enum {
     DM_DEFAULT,
     DM_DEBUG
@@ -50,6 +54,8 @@ typedef enum {
 DrawMode drawmode = DM_DEFAULT;
 
 void drawBoid(Vector3 pos, Vector3 dir) {
+    TracyCZoneN(dboidctx, "Draw Boid", 1);
+
     if (drawmode == DM_DEBUG) {
         DrawLine3D(pos, Vector3Add(pos, dir), RED);
     }
@@ -57,11 +63,15 @@ void drawBoid(Vector3 pos, Vector3 dir) {
     Vector3 end = Vector3Add(pos, Vector3Scale(Vector3Normalize(dir), 0.5f));
     DrawCylinderWiresEx(pos, end, BOIDS_START_RAD, BOIDS_END_RAD, BOIDS_SIDES, BLACK);
     DrawCylinderEx(pos, end, BOIDS_START_RAD, BOIDS_END_RAD, BOIDS_SIDES, BLUE);
+
+    TracyCZoneEnd(dboidctx);
 }
 
 //TODO: Setup grid for faster distance lookups
 // Recreate grid each frame? limit the amount of other boids to check
 Vector3 calcCenterRule(size_t idx) {
+    TracyCZoneN(ctx, "Calc Center Rule", 1);
+
     Vector3 total = { 0.f, 0.f, 0.f };
 
     for (size_t i = 0; i < NUM_BOIDS; ++i) {
@@ -70,10 +80,13 @@ Vector3 calcCenterRule(size_t idx) {
     }
     total = Vector3Scale(total, 1.f / (NUM_BOIDS - 1));
 
+    TracyCZoneEnd(ctx);
     return Vector3Scale(Vector3Subtract(total, boids.pos[idx]), CENTER_FACTOR);
 }
 
 Vector3 calcDistRule(size_t idx) {
+    TracyCZoneN(ctx, "Calc Dist Rule", 1);
+
     Vector3 total = { 0.f, 0.f, 0.f };
     for (size_t i = 0; i < NUM_BOIDS; ++i) {
         if (i == idx) continue;
@@ -83,10 +96,14 @@ Vector3 calcDistRule(size_t idx) {
             total = Vector3Add(total, diff);
         }
     }
+
+    TracyCZoneEnd(ctx);
     return Vector3Scale(total, AVOID_FACTOR);
 }
 
 Vector3 calcMatchRule(size_t idx) {
+    TracyCZoneN(ctx, "Calc Match Rule", 1);
+
     Vector3 total = { 0.f, 0.f, 0.f };
 
     for (size_t i = 0; i < NUM_BOIDS; ++i) {
@@ -95,10 +112,13 @@ Vector3 calcMatchRule(size_t idx) {
     }
     total = Vector3Scale(total, 1.f / (NUM_BOIDS - 1));
 
+    TracyCZoneEnd(ctx);
     return Vector3Scale(Vector3Subtract(total, boids.vel[idx]), MATCH_FACTOR);
 }
 
 void applyBoundRule(size_t idx) {
+    TracyCZoneN(ctx, "Apply Bound Rule", 1);
+
     Vector3 *p = &boids.pos[idx];
     Vector3 *v = &boids.vel[idx];
 
@@ -122,14 +142,20 @@ void applyBoundRule(size_t idx) {
     if (p->z > boundSize.x/2.f) {
         v->z -= TURN_FACTOR;
     }
+
+    TracyCZoneEnd(ctx);
 }
 
 void applySpeedRule(size_t idx) {
+    TracyCZoneN(ctx, "Apply Speed Rule", 1);
+
     float len = Vector3Length(boids.vel[idx]);
 
     if (len > MAX_SPEED) {
         boids.vel[idx] = Vector3Scale(Vector3Normalize(boids.vel[idx]), MAX_SPEED);
     }
+
+    TracyCZoneEnd(ctx);
 }
 
 void InitBoidsApp() {
@@ -157,10 +183,19 @@ void InitBoidsApp() {
         boids.vel[i] = (Vector3) { rx, ry, rz };
     }
     print_boids(boids);
+
+    arr = calloc(count, sizeof(int));
+    TracyCAlloc(arr, count * sizeof(int));
+
+    for (size_t i = 0; i < count; ++i) {
+        TraceLog(LOG_INFO, "We got a value -> arr[%zu] = %zu", i, arr[i]);
+    }
 }
 
 void TickBoidsApp() {
+    TracyCZoneN(uctx, "Update Camera", 1);
     UpdateCamera(&camera, CAMERA_THIRD_PERSON);
+    TracyCZoneEnd(uctx);
 
     if (IsKeyPressed(KEY_R)) {
         //TODO: Handle mode draw modes later?
@@ -168,6 +203,7 @@ void TickBoidsApp() {
         else if (drawmode == DM_DEBUG) drawmode = DM_DEFAULT;
     }
 
+    TracyCZoneN(bctx, "Tick Boids Positions", 1);
     for (size_t i = 0; i < NUM_BOIDS; ++i) {
         boids.vel[i] = Vector3Add(boids.vel[i], calcCenterRule(i));
         boids.vel[i] = Vector3Add(boids.vel[i], calcDistRule(i));
@@ -177,11 +213,13 @@ void TickBoidsApp() {
 
         boids.pos[i] = Vector3Add(boids.pos[i], boids.vel[i]);
     }
+    TracyCZoneEnd(bctx);
 }
 
 void DrawBoidsApp() {
     GuiStatusBar(RECT(0, 585, 800, 15), TextFormat("simulating %zu boids; %zu FPS", NUM_BOIDS, GetFPS()));
 
+    TracyCZoneN(ctx, "Draw 3D", 1);
     BeginMode3D(camera);
     for (size_t i = 0; i < NUM_BOIDS; ++i) {
         //TODO: Look into instanciating the meshes to render in one draw call
@@ -192,6 +230,7 @@ void DrawBoidsApp() {
     DrawCubeWiresV(camTarget, boundSize, GREEN);
     DrawCubeV(camTarget, boundSize, ColorAlpha(GREEN, 0.15f));
     EndMode3D();
+    TracyCZoneEnd(ctx);
 
     if (drawmode == DM_DEBUG) {
         for (size_t i = 0; i < NUM_BOIDS; ++i) {
@@ -206,4 +245,8 @@ void DrawBoidsApp() {
 
 void DropBoidsApp() {
     TraceLog(LOG_DEBUG, "[BOIDS] dropping boids viewer application\n");
+
+    TracyCFree(arr);
+    free(arr);
+    arr = NULL;
 }
