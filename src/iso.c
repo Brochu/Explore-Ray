d685a7f38598654ed703bcf52d3ed6403616045f
Brochu <alexbrochu1@gmail.com>
2024-10-24 21:08:11 -0400

    [DEV] WIP: Added some debug info

--- src/iso.c
+++ src/iso.c
@@ -2,8 +2,10 @@
 
 #include "raylib.h"
 #include "raygui.h"
+#include "raymath.h"
 
 #define RECT(x, y, w, h) ((Rectangle){x, y, w, h})
+#define VECPOS(v) (int)v.x, (int)v.y
 #define MAP_DIMS 25
 
 typedef struct {
@@ -25,6 +27,7 @@ sheet_t floors = { 0, 7, 160, 80, 29, 5 }; // floors.png
 sheet_t exits = { 0, 493, 160, 80, 8, 5 }; // floors.png
 sheet_t walls = { 0, 13, 108, 226, 44, 17 }; // walls.png
 sheet_t char_n = { 0, 1642, 98, 90, 128, 8 }; // mainchar.gif
+sheet_t char_r = { 0, 0, 103, 90, 128, 8 }; // run.png
 
 typedef size_t tile_t;
 tile_t map[MAP_DIMS * MAP_DIMS] = { 0 };
@@ -35,7 +38,7 @@ Texture2D chartex;
 void InitIsoApp() {
     SetTraceLogLevel(LOG_DEBUG);
     TraceLog(LOG_DEBUG, "[ISO] Starting isometric viewer application");
-    chartex = LoadTexture("isodata\\mainchar.gif");
+    chartex = LoadTexture("isodata\\run.png");
 }
 
 void TickIsoApp() {
@@ -48,13 +51,21 @@ void DrawIsoApp() {
         TextFormat("highlight pos (%i, %i); %zu FPS", hover.x, hover.y, GetFPS())
     );
 
+    Vector2 center = { .x = (float)GetScreenWidth()/2, .y = (float)GetScreenHeight()/2 };
+    Vector2 pos = Vector2Subtract(center, (Vector2) { .x = (float)char_r.width/2, .y = (float)char_r.height/2 });
+    Vector2 mpos = GetMousePosition();
+    Vector2 txtpos = Vector2Add(mpos, (Vector2){ .x = 0, .y = -20 });
+    DrawCircle(VECPOS(mpos), 5.f, RED);
+    DrawLine(VECPOS(center), VECPOS(mpos), BLUE);
+    DrawText("HEY", VECPOS(txtpos), 15, WHITE);
+
     Rectangle rect = {
-        .x = (float)char_n.xoffset,
-        .y = (float)char_n.yoffset + (char_n.height * (findex % (char_n.size / char_n.stride))),
-        .width = (float)char_n.width,
-        .height = (float)char_n.height
+        .x = (float)char_r.xoffset + (char_r.width * ((findex / 6) % char_r.stride)),
+        .y = (float)char_r.yoffset,
+        .width = (float)char_r.width,
+        .height = (float)char_r.height
     };
-    DrawTextureRec(chartex, rect, (Vector2){ .x = 0, .y = 0 }, WHITE);
+    DrawTextureRec(chartex, rect, pos, WHITE);
     findex++;
 
     //TODO: Draw map here
